036fe69187d5d30000b476ee6f6833fedd09cf27
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2025-01-22 17:54:20 +0900

    init

---
+++ src/main/java/greedy/greedybot/GreedyBotApplication.java
@@ -0,0 +1,13 @@
+package greedy.greedybot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreedyBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreedyBotApplication.class, args);
+    }
+
+}
