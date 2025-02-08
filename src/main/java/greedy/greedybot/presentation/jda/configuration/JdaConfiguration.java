aa96403f92d28ee823a1280e72e631166d20b097
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2025-02-08 14:49:31 +0900

    chore: 그리디 서버(길드) 빈 등록

--- src/main/java/greedy/greedybot/presentation/jda/configuration/JdaConfiguration.java
+++ src/main/java/greedy/greedybot/presentation/jda/configuration/JdaConfiguration.java
@@ -1,10 +1,12 @@
 package greedy.greedybot.presentation.jda.configuration;
 
+import greedy.greedybot.presentation.jda.listener.StatusCommandListener;
 import java.util.EnumSet;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -16,8 +18,17 @@ public class JdaConfiguration {
     @Value("${discord.token}")
     private String token;
 
+    @Value("${discord.guild_id}")
+    private String guildId;
+
+    private final StatusCommandListener statusCommandListener;
+
+    public JdaConfiguration(final StatusCommandListener statusCommandListener) {
+        this.statusCommandListener = statusCommandListener;
+    }
+
     @Bean
-    JDA jda() {
+    JDA jda() throws InterruptedException {
         // ref https://ci.dv8tion.net/job/JDA/javadoc/net/dv8tion/jda/api/requests/GatewayIntent.html
         final EnumSet<GatewayIntent> intents = EnumSet.of(
                 GatewayIntent.GUILD_MESSAGES,
@@ -27,9 +38,16 @@ public class JdaConfiguration {
         );
 
         return JDABuilder.createLight(token)
-                .setActivity(Activity.listening("메세지 입력 대기"))
+                .setActivity(Activity.listening("메세지 입력"))
                 .setStatus(OnlineStatus.ONLINE)
+                .addEventListeners(statusCommandListener)
                 .enableIntents(intents)
-                .build();
+                .build()
+                .awaitReady(); // https://ci.dv8tion.net/job/JDA/javadoc/net/dv8tion/jda/api/JDABuilder.html#build()
+    }
+
+    @Bean
+    Guild greedyGuild(JDA jda) {
+        return jda.getGuildById(guildId);
     }
 }
--- src/test/java/greedy/greedybot/presentation/jda/configuration/JdaConfigurationTest.java
+++ src/test/java/greedy/greedybot/presentation/jda/configuration/JdaConfigurationTest.java
@@ -3,6 +3,7 @@ package greedy.greedybot.presentation.jda.configuration;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.Guild;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,8 +14,12 @@ class JdaConfigurationTest {
     @Autowired
     private JDA jda;
 
+    @Autowired
+    private Guild greedyGuild;
+
     @Test
     void setup_jda() {
         assertThat(jda).isNotNull();
+        assertThat(greedyGuild).isNotNull();
     }
 }
