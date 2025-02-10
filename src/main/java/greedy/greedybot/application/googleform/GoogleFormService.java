9019c6f3f762dd54b66a584ef100d05d2ef49038
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2025-02-10 12:49:48 +0900

    feat: 구글폼 저장 로직 추가

--- src/main/java/greedy/greedybot/application/googleform/GoogleFormService.java
+++ src/main/java/greedy/greedybot/application/googleform/GoogleFormService.java
@@ -2,6 +2,8 @@ package greedy.greedybot.application.googleform;
 
 import greedy.greedybot.application.googleform.dto.EnrollFormWatchResult;
 import greedy.greedybot.application.googleform.dto.client.GoogleFormInformationResponse;
+import greedy.greedybot.domain.form.GoogleFormWatch;
+import greedy.greedybot.domain.form.GoogleFormWatchDiscordRepository;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,17 +11,26 @@ public class GoogleFormService {
 
     private final GoogleCredential googleCredential;
     private final GoogleFormApiClient googleFormApiClient;
+    private final GoogleFormWatchDiscordRepository googleFormWatchDiscordRepository;
 
-    public GoogleFormService(final GoogleCredential googleCredential, final GoogleFormApiClient googleFormApiClient) {
+    public GoogleFormService(final GoogleCredential googleCredential,
+                             final GoogleFormApiClient googleFormApiClient,
+                             final GoogleFormWatchDiscordRepository googleFormWatchDiscordRepository) {
         this.googleCredential = googleCredential;
         this.googleFormApiClient = googleFormApiClient;
+        this.googleFormWatchDiscordRepository = googleFormWatchDiscordRepository;
     }
 
     // TODO: test
     public EnrollFormWatchResult enrollFormWatch(String formId) {
-        String accessToken = googleCredential.getAccessToken();
-        GoogleFormInformationResponse googleFormInformationResponse = googleFormApiClient.readForm(formId, accessToken);
-        int responseCount = googleFormApiClient.readFormResponseCount(formId, accessToken);
-        return new EnrollFormWatchResult(googleFormInformationResponse.title(), responseCount);
+        googleFormWatchDiscordRepository.findByFormId(formId).ifPresent(googleFormWatch -> {
+            throw new RuntimeException("이미 등록된 구글폼 감지기입니다");
+        });
+        final String accessToken = googleCredential.getAccessToken();
+        final GoogleFormInformationResponse formInformationResponse = googleFormApiClient.readForm(formId, accessToken);
+        final int responseCount = googleFormApiClient.readFormResponseCount(formId, accessToken);
+        final GoogleFormWatch formWatch = new GoogleFormWatch(formId, formInformationResponse.title(), responseCount);
+        googleFormWatchDiscordRepository.saveGoogleFormWatch(formWatch);
+        return new EnrollFormWatchResult(formInformationResponse.title(), responseCount);
     }
 }
