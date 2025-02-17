91ab711327f22b1f28a95c1935c59141010a0656
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2025-02-17 11:25:06 +0900

    style: reformat

--- src/main/java/greedy/greedybot/application/googleform/dto/client/GoogleFormInformationResponse.java
+++ src/main/java/greedy/greedybot/application/googleform/dto/client/GoogleFormInformationResponse.java
@@ -3,14 +3,14 @@ package greedy.greedybot.application.googleform.dto.client;
 public record GoogleFormInformationResponse(
         Info info
 ) {
+    public String title() {
+        return info.title();
+    }
+
     record Info(
             String title,
             String description,
             String documentTitle
     ) {
-    }
-
-    public String title() {
-        return info.title();
     }
 }
