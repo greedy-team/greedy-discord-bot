4595b87e40a98bec18e11b7989963c52295a1aeb
haeyoon1 <hynam2002@gmail.com>
2025-02-28 03:54:07 +0900

    feat: 예약 메세지 스케줄링 기능 추가

---
+++ src/main/java/greedy/greedybot/scheduledMessage/jda/listener/ScheduledMessageScheduler.java
@@ -0,0 +1,45 @@
+package greedy.greedybot.scheduledMessage.jda.listener;
+
+import greedy.greedybot.presentation.jda.listener.StatusCommandListener;
+import greedy.greedybot.scheduledMessage.domain.ScheduledMessage;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+@Component
+public class ScheduledMessageScheduler {
+
+    private final Logger log = LoggerFactory.getLogger(StatusCommandListener.class);
+    private final JDA jda;
+
+    public ScheduledMessageScheduler(@Lazy JDA jda) {
+        this.jda = jda;
+    }
+
+    public void schedule(ScheduledMessage scheduledMessage, Runnable runnable) {
+        Long delay = scheduledMessage.getScheduledTime().atZone(java.time.ZoneId.systemDefault()).toInstant().toEpochMilli() - System.currentTimeMillis();
+
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                sendScheduledMessage(scheduledMessage);
+                runnable.run();
+            }
+        }, delay);
+    }
+
+    private void sendScheduledMessage(ScheduledMessage scheduledMessage) {
+        TextChannel channel = jda.getTextChannelById(scheduledMessage.getChannelId());
+
+        channel.sendMessage(scheduledMessage.getContent()).queue();
+        log.info("📢 예약된 메시지가 Discord 채널({})에 전송됨: {}", scheduledMessage.getContent(), scheduledMessage.getContent());
+
+    }
+}
