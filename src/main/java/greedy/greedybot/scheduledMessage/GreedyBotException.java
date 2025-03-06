1ecfa37875e7da9db3374961729c92ee6c74e15c
haeyoon1 <hynam1220@naver.com>
2025-03-06 18:27:53 +0900

    refactor: ScheduledCommandListener 클래스 수정 - 함수화 및 상수화

---
+++ src/main/java/greedy/greedybot/scheduledMessage/GreedyBotException.java
@@ -0,0 +1,10 @@
+package greedy.greedybot.scheduledMessage;
+
+public class GreedyBotException extends RuntimeException {
+    public GreedyBotException(String message) {
+        super(message);
+    }
+    public GreedyBotException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
