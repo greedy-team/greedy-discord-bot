977c5368568b83bd1783d015751b758fda432e14
haeyoon1 <hynam1220@naver.com>
2025-03-06 18:27:53 +0900

    refactor: ScheduledCommandListener 클래스 수정 - 함수화 및 상수화

--- src/main/java/greedy/greedybot/scheduledMessage/jda/listener/ScheduledCommandListener.java
+++ src/main/java/greedy/greedybot/scheduledMessage/jda/listener/ScheduledCommandListener.java
@@ -2,10 +2,9 @@ package greedy.greedybot.scheduledMessage.jda.listener;
 
 import greedy.greedybot.presentation.jda.listener.SlashCommandListener;
 import greedy.greedybot.presentation.jda.listener.StatusCommandListener;
+import greedy.greedybot.scheduledMessage.GreedyBotException;
 import greedy.greedybot.scheduledMessage.domain.ScheduledMessage;
 import greedy.greedybot.scheduledMessage.domain.ScheduledMessageService;
-import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
@@ -13,7 +12,6 @@ import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -26,6 +24,8 @@ public class ScheduledCommandListener implements SlashCommandListener {
 
     private final ScheduledMessageService scheduledMessageService;
     private final Logger log = LoggerFactory.getLogger(StatusCommandListener.class);
+
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm:ss", Locale.ENGLISH);
 
     public ScheduledCommandListener(ScheduledMessageService scheduledMessageService) {
         this.scheduledMessageService = scheduledMessageService;
@@ -45,36 +45,45 @@ public class ScheduledCommandListener implements SlashCommandListener {
 
     @Override
     public void onAction(@NotNull SlashCommandInteractionEvent event) {
-        String message = event.getOption("message").getAsString();
-        String timeString = event.getOption("time").getAsString();
-        String channelId = event.getChannelId();
-
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm", Locale.ENGLISH);
-        LocalDateTime time;
-
-        //1. 시간 형식 검증
         try {
-            time = LocalDateTime.parse(timeString, formatter);
+            final String message = event.getOption("message").getAsString();
+            final String timeString = event.getOption("time").getAsString();
+            final String channelId = event.getChannelId();
+
+            // 1. 시간 형식 검증
+            LocalDateTime time = parseScheduledTime(timeString);
+
+            // 2. 과거 시간 입력 여부 확인
+            IsValidscheduledTime(time);
+
+            log.info("예약된 메시지: {}", message);
+            log.info("예약된 시간: {}", time);
+
+            final ScheduledMessage scheduledMessage = new ScheduledMessage(message, time, event.getUser().getId(), channelId);
+            scheduledMessageService.scheduleMessage(scheduledMessage);
+
+            event.reply("✅ 메시지가 " + timeString + "에 예약되었습니다.").queue();
+        } catch (GreedyBotException e) {
+            log.error(e.getMessage());
+            event.reply(e.getMessage()).setEphemeral(true).queue();
+        }
+    }
+
+    public LocalDateTime parseScheduledTime(String timeString) {
+        try {
+           return LocalDateTime.parse(timeString, DATE_TIME_FORMATTER);
         } catch (DateTimeParseException e) {
             log.error("❌ 잘못된 시간 형식: {}", timeString);
-            event.reply("❌ 잘못된 시간 형식입니다! yyyy-MM-dd HH:mm 형식으로 입력해주세요.").setEphemeral(true).queue();
-            return;
+            throw new GreedyBotException("❌ 잘못된 시간 형식입니다! yyyy-MM-dd HH:mm 형식으로 입력해주세요.");
         }
+    }
 
-        //2. 과거 시간 입력 여부 확인
-        LocalDateTime now = LocalDateTime.now();
+    public void IsValidscheduledTime(LocalDateTime time) {
+        final LocalDateTime now = LocalDateTime.now();
+
         if (time.isBefore(now)) {
-            log.error("❌ 잘못된 예약 시간: {} (과거 시간 입력됨)", timeString);
-            event.reply("❌ 예약할 시간은 현재 시간 이후여야 합니다!").setEphemeral(true).queue();
-            return;
+            log.error("❌ 잘못된 예약 시간: {} (과거 시간 입력됨)", time);
+            throw new GreedyBotException("❌ 예약할 시간은 현재 시간 이후여야 합니다!");
         }
-
-        log.info("예약된 메시지: {}", message);
-        log.info("예약된 시간: {}", time);
-
-        ScheduledMessage scheduledMessage = new ScheduledMessage(message, time, event.getUser().getId(), channelId);
-        scheduledMessageService.scheduleMessage(scheduledMessage);
-
-        event.reply("✅ 메시지가 " + timeString + "에 예약되었습니다.").queue();
     }
 }
