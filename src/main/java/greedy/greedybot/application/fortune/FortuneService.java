9fa84f2da98d2cd6e04fcec7da7df112d0a4c911
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2025-03-18 15:41:15 +0900

    fix: 오늘의 운세 랜덤 분포도 개선

--- src/main/java/greedy/greedybot/application/fortune/FortuneService.java
+++ src/main/java/greedy/greedybot/application/fortune/FortuneService.java
@@ -1,7 +1,10 @@
 package greedy.greedybot.application.fortune;
 
 import greedy.greedybot.domain.fortune.FortuneStaticRepository;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.time.LocalDate;
+import java.util.Objects;
 import java.util.Random;
 import org.springframework.stereotype.Service;
 
@@ -16,8 +19,20 @@ public class FortuneService {
 
     public String findTodayFortuneByKey(final Long userId, final LocalDate today) {
         final int fortuneSize = fortuneRepository.getSize();
-        final long randomSeed = userId + today.toEpochDay();
+        final long randomSeed = getSeed(userId, today.toEpochDay());
         final int todayFortuneIndex = new Random(randomSeed).nextInt(fortuneSize);
         return fortuneRepository.getFortuneByIndex(todayFortuneIndex);
+    }
+
+    public int getSeed(final Long userId, final Long today) {
+        final int fortuneSize = fortuneRepository.getSize();
+        try {
+            final MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(String.valueOf(today + userId).getBytes());
+            final String encrypt = new String(md.digest());
+            return new Random(Objects.hash(encrypt)).nextInt(fortuneSize);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("SHA-256 algorithm not found", e);
+        }
     }
 }
