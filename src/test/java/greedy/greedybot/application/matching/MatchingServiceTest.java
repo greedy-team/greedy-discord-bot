f9e68138afece4ba3ed7e733f4e762f046b95684
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2025-05-05 23:25:54 +0900

    refactor: 동일한 이름의 리뷰이-리뷰어 매칭이 되지 않도록 수정

--- src/test/java/greedy/greedybot/application/matching/MatchingServiceTest.java
+++ src/test/java/greedy/greedybot/application/matching/MatchingServiceTest.java
@@ -1,13 +1,17 @@
 package greedy.greedybot.application.matching;
 
 import greedy.greedybot.application.matching.dto.MatchingResult;
+import greedy.greedybot.common.exception.GreedyBotException;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class MatchingServiceTest {
     private MatchingService matchingService;
@@ -30,8 +34,8 @@ public class MatchingServiceTest {
         //then
         final String announcement = matchingResult.toDiscordAnnouncement();
         assertThat(announcement).contains("리뷰이1    ->   리뷰어1");
-        assertThat(announcement).contains("리뷰이2    ->   리뷰어1");
-        assertThat(announcement).contains("리뷰이3    ->   리뷰어2");
+        assertThat(announcement).contains("리뷰이2    ->   리뷰어2");
+        assertThat(announcement).contains("리뷰이3    ->   리뷰어1");
         assertThat(announcement).contains("리뷰이4    ->   리뷰어2");
     }
 
@@ -50,4 +54,34 @@ public class MatchingServiceTest {
         assertThat(announcement).contains("리뷰이1    ->   리뷰어1");
         assertThat(announcement).contains("리뷰이2    ->   리뷰어2");
     }
+
+    @RepeatedTest(20)
+    @DisplayName("리뷰어와 리뷰이가 같을때 매칭 테스트")
+    void testMatchStudyDoesNotMatchSamePerson() {
+        //given
+        final List<String> reviewees = List.of("태연", "해윤") ;
+        final List<String> reviewers = List.of("태연", "해윤");
+
+        //when
+        final MatchingResult matchingResult = matchingService.matchStudy(reviewees, reviewers);
+
+        //then
+        final String announcement = matchingResult.toDiscordAnnouncement();
+        assertThat(announcement).doesNotContain("태연    ->   태연");
+        assertThat(announcement).doesNotContain("해윤    ->   해윤");
+    }
+
+    @Test
+    @DisplayName("경우의 수가 존재하지 않을때 매칭 테스트")
+    void testMatchStudyDoesNotMatchNoCases() {
+        //given
+        final List<String> reviewees = List.of("태연", "승준");
+        final List<String> reviewers = List.of("태연", "태연"); // 동일한 이름의 리뷰이-리뷰어 매칭이 무조건 발생
+
+        //when & then
+        assertThatThrownBy(() -> matchingService.matchStudy(reviewees, reviewers))
+                .isInstanceOf(GreedyBotException.class)
+                .hasMessageContaining("매칭 경우의 수가 존재 하지 않습니다");
+    }
+
 }
