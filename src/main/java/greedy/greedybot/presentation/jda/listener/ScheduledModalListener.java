80f134b06f6256bee7598e5e6f72b892b590567f
haeyoon1 <hynam1220@naver.com>
2025-09-17 15:04:38 +0900

    feat: 시간, 텍스트 입력 -> ModalListener에서 관리

---
+++ src/main/java/greedy/greedybot/presentation/jda/listener/ScheduledModalListener.java
@@ -0,0 +1,83 @@
+package greedy.greedybot.presentation.jda.listener;
+
+import greedy.greedybot.application.message.ScheduledMessageService;
+import greedy.greedybot.common.exception.GreedyBotException;
+import greedy.greedybot.domain.message.ScheduledMessage;
+import greedy.greedybot.presentation.jda.role.ScheduledMessageChannel;
+import greedy.greedybot.presentation.jda.role.ScheduledMessageChannels;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Locale;
+import net.dv8tion.jda.api.events.interaction.ModalInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduledModalListener extends ListenerAdapter {
+
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm",
+        Locale.ENGLISH);
+
+    private final ScheduledMessageService scheduledMessageService;
+    private final ScheduledMessageChannels scheduledMessageChannels;
+    private static final Logger log = LoggerFactory.getLogger(ScheduledModalListener.class);
+
+    public ScheduledModalListener(ScheduledMessageService scheduledMessageService,
+        ScheduledMessageChannels scheduledMessageChannels) {
+        this.scheduledMessageService = scheduledMessageService;
+        this.scheduledMessageChannels = scheduledMessageChannels;
+    }
+
+    @Override
+    public void onModalInteraction(@NotNull ModalInteractionEvent event) {
+        try {
+            String[] parts = event.getModalId().split(":", 2);
+            final ScheduledMessageChannel channelEnum = ScheduledMessageChannel.valueOf(parts[1]);
+            final long resolvedChannelId = scheduledMessageChannels.getChannelId(channelEnum);
+
+            final String message = event.getValue("message").getAsString();
+            final String timeString = event.getValue("time").getAsString();
+
+            // 시간 관련 검증
+            LocalDateTime time = parseScheduledTime(timeString); //형식 검증
+            isValidScheduledTime(time); //과거 시간 입력 검증
+
+            final ScheduledMessage scheduledMessage = new ScheduledMessage(
+                message,
+                time,
+                event.getUser().getId(),
+                String.valueOf(resolvedChannelId)
+            );
+
+            scheduledMessageService.scheduleMessage(scheduledMessage);
+
+            log.info("✅ 메시지가 " + resolvedChannelId + " 채널에 예약되었습니다.");
+            event.reply("✅ 메시지가 " + timeString + "에 예약되었습니다.").queue();
+        } catch (GreedyBotException e) {
+            log.error(e.getMessage());
+            event.reply(e.getMessage()).setEphemeral(true).queue();
+        }
+    }
+
+    public LocalDateTime parseScheduledTime(String timeString) {
+        try {
+            return LocalDateTime.parse(timeString, DATE_TIME_FORMATTER);
+        } catch (DateTimeParseException e) {
+            log.error("❌ 잘못된 시간 형식: {}", timeString);
+            throw new GreedyBotException("❌ 잘못된 시간 형식입니다! yyyy-MM-dd HH:mm 형식으로 입력해주세요.");
+        }
+    }
+
+    public void isValidScheduledTime(LocalDateTime time) {
+        final LocalDateTime now = LocalDateTime.now();
+
+        if (time.isBefore(now)) {
+            log.error("❌ 잘못된 예약 시간: {} (과거 시간 입력됨)", time);
+            throw new GreedyBotException("❌ 예약할 시간은 현재 시간 이후여야 합니다!");
+        }
+    }
+}
