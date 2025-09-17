4968b4d212fb0a11c96b31e985f5de9cbfc040c9
haeyoon1 <hynam1220@naver.com>
2025-09-17 15:07:27 +0900

    feat: 모달, 드롭다운 선택하는 리스너 생성

---
+++ src/main/java/greedy/greedybot/presentation/jda/listener/ScheduledMessageSelectListener.java
@@ -0,0 +1,44 @@
+package greedy.greedybot.presentation.jda.listener;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import net.dv8tion.jda.api.events.interaction.component.StringSelectInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.components.text.TextInput;
+import net.dv8tion.jda.api.interactions.components.text.TextInputStyle;
+import net.dv8tion.jda.api.interactions.modals.Modal;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduledMessageSelectListener extends ListenerAdapter {
+
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm",
+        Locale.ENGLISH);
+
+    @Override
+    public void onStringSelectInteraction(@NotNull StringSelectInteractionEvent event) {
+        if (!event.getComponentId().equals("scheduled-channel-modal")) {
+            return;
+        }
+
+        TextInput messageInput = TextInput.create("message", "메시지", TextInputStyle.PARAGRAPH)
+            .setRequired(true)
+            .build();
+
+        final String defaultTime = LocalDateTime.now().format(DATE_TIME_FORMATTER);
+        TextInput timeInput = TextInput.create("time", "예약 시간", TextInputStyle.SHORT)
+            .setValue(defaultTime)
+            .setRequired(true)
+            .build();
+
+        final String channelId = event.getValues().get(0);
+        Modal modal = Modal.create("scheduled-message-modal:" + channelId, "예약 메시지 등록")
+            .addActionRow(messageInput)
+            .addActionRow(timeInput)
+            .build();
+
+        event.replyModal(modal).queue();
+    }
+}
